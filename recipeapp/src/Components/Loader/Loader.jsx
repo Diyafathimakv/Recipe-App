5039a87e2f0d42a2aec0c7837b5f1b8cba8e31cd
Diya Fathima <diludiya83@gmail.com>
2026-05-09 13:13:03 +0530

    made edits in home,category and favorites

---
+++ recipeapp/src/Components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+      <p>Cooking something delicious...</p>
+    </div>
+  );
+};
+
+export default Loader;
